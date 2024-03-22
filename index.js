696bff2983f7b10be16426f7926dbeb7ac662f0e
gianlucafiore <gfiorepersonal@gmail.com>
2024-03-22 11:40:57 -0300

    preformatting

--- index.js
+++ index.js
@@ -36,22 +36,34 @@ const {
         name: "colorLight",
         label: "Color Light",
         type: "Color"
-      }
+      },
+      {
+        name: "preformatting",
+        label: "String formatting with JS",
+        type: "String"
+      },
+
     ],
     run: (value, req, attrs, cls) => {
-    const rndid = Math.floor(Math.random() * 16777215).toString(16);
+      const rndid = Math.floor(Math.random() * 16777215).toString(16);
+      let value_formated;
+      if(attrs.preformatting){
+        value_formated.replace('{{{value}}}', value);
+      }
+      else value_formated = value;
       const opts = {
         width: attrs.width,
         height: attrs.height,
         colorDark: attrs.colorDark || 'black',
-        colorLight:  attrs.colorLight || 'white'
+        colorLight:  attrs.colorLight || 'white',
+        preformatting: value_formated
       }; 
       return (
         `<div id="${rndid}"></div>`
         +
         script(
           domReady(`new QRCode(document.getElementById("${rndid}"), {
-            text: '${value}',
+            text: '${opts.value_formated}',
             width: '${opts.width || 128}',
             height: '${opts.height || 128}',
             colorDark : '${opts.colorDark || "#000000"}',
